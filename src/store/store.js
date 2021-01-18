4fd258ca934e3f68e8cd55ea49e6fe09f00b2a6c
Can <canyil@gmail.com>
2021-01-18 16:48:03 +0100

    fix merge conflict

--- src/store/store.js
+++ src/store/store.js
@@ -10,67 +10,61 @@ Vue.use(Vuex);
 
 const graphqlEndpoint = 'https://dylen-ego-network-service.acdh-dev.oeaw.ac.at/graphql'
 
-const store = new Vuex.Store({
-  state: {
-    availableQueryParams: [],
-    selectedCorpus: {id: '', name: '', sources: []},
-    selectedSubcorpus: {id: '', name: '', targetWords: []},
-    selectedTargetword: {id: '', text: ''},
-    egoNetworks: [],
-  },
-  mutations: {
-    changeSelectedCorpus (state, corpus) {
-      if (corpus) {
-        state.selectedCorpus = corpus;
-      } else {
-        state.selectedCorpus = state.availableQueryParams[0];
-      }
-      this.commit('changeSelectedSubcorpus', false);
+const mainModule = {
+    namespaced: true,
+    state: {
+        availableQueryParams: [],
+        selectedCorpus: {id: '', name: '', sources: []},
+        selectedSubcorpus: {id: '', name: '', targetWords: []},
+        selectedTargetword: {id: '', text: ''},
+        egoNetworks: [],
     },
-    changeSelectedSubcorpus (state, subcorpus) {
-      if (subcorpus) {
-        state.selectedSubcorpus = subcorpus;
-      } else {
-        state.selectedSubcorpus = state.selectedCorpus.sources[0];
-      }
-      this.commit('changeSelectedTargetword', false);
-    },
-    changeSelectedTargetword (state, targetword) {
-      if (targetword) {
-        state.selectedTargetword = targetword;
-      } else {
-        state.selectedTargetword = state.selectedSubcorpus.targetWords[0];
-      }
+    mutations: {
+        changeSelectedCorpus (state, corpus) {
+            if (corpus) {
+                state.selectedCorpus = corpus;
+            } else {
+                state.selectedCorpus = state.availableQueryParams[0];
+            }
+            this.commit('changeSelectedSubcorpus', false);
+        },
+        changeSelectedSubcorpus (state, subcorpus) {
+            if (subcorpus) {
+                state.selectedSubcorpus = subcorpus;
+            } else {
+                state.selectedSubcorpus = state.selectedCorpus.sources[0];
+            }
+            this.commit('changeSelectedTargetword', false);
+        },
+        changeSelectedTargetword (state, targetword) {
+            if (targetword) {
+                state.selectedTargetword = targetword;
+            } else {
+                state.selectedTargetword = state.selectedSubcorpus.targetWords[0];
+            }
+        },
+        addEgoNetwork (state, networkObj) {
+            state.egoNetworks.push(networkObj);
+        },
+        removeEgoNetwork(state, networkID) {
+            const selectedNetworkIndex = state.egoNetworks.findIndex(obj => {
+                return obj.text === networkID;
+            });
+            state.egoNetworks.splice(selectedNetworkIndex, 1);
+        }
     },
     getters: {
         availableQueryParams: (state) => state.availableQueryParams,
         selectedCorpus: (state) => state.selectedCorpus,
         selectedSubcorpus: (state) => state.selectedSubcorpus,
         selectedTargetword: (state) => state.selectedTargetword,
-        selectedCorpusID: (state) => state.selectedCorpusID,
-        selectedSubcorpusID: (state) => state.selectedSubcorpusID,
-        selectedTargetwordID: (state) => state.selectedTargetwordID,
         egoNetworks: (state) => state.egoNetworks,
     },
-    removeEgoNetwork(state, networkID) {
-      const selectedNetworkIndex = state.egoNetworks.findIndex(obj => {
-        return obj.text === networkID;
-      });
-      state.egoNetworks.splice(selectedNetworkIndex, 1);
-    }
-  },
-  getters: {
-    availableQueryParams: (state) => state.availableQueryParams,
-    selectedCorpus: (state) => state.selectedCorpus,
-    selectedSubcorpus: (state) => state.selectedSubcorpus,
-    selectedTargetword: (state) => state.selectedTargetword,
-    egoNetworks: (state) => state.egoNetworks,
-  },
-  actions: {
-    async loadAvailableQueryParams({state}) {
-      try {
-        const graphqlQuery = {
-          query: `{
+    actions: {
+        async loadAvailableQueryParams({state}) {
+            try {
+                const graphqlQuery = {
+                    query: `{
             allAvailableCorpora {
               id
               name
@@ -88,20 +82,20 @@ const store = new Vuex.Store({
               }
             }
           }`
-        };
-        const response = await axios.post(graphqlEndpoint, graphqlQuery);
-        state.availableQueryParams = response.data.data.allAvailableCorpora;
-        state.selectedCorpus = response.data.data.allAvailableCorpora[0];
-        state.selectedSubcorpus = response.data.data.allAvailableCorpora[0].sources[0];
-        state.selectedTargetword = response.data.data.allAvailableCorpora[0].sources[0].targetWords[0];
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async loadEgoNetwork({state}) {
-      try {
-        const graphqlQuery = {
-          query: `{
+                };
+                const response = await axios.post(graphqlEndpoint, graphqlQuery);
+                state.availableQueryParams = response.data.data.allAvailableCorpora;
+                state.selectedCorpus = response.data.data.allAvailableCorpora[0];
+                state.selectedSubcorpus = response.data.data.allAvailableCorpora[0].sources[0];
+                state.selectedTargetword = response.data.data.allAvailableCorpora[0].sources[0].targetWords[0];
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async loadEgoNetwork({state}) {
+            try {
+                const graphqlQuery = {
+                    query: `{
             getNetwork(targetword_id: "${state.selectedTargetword.id}", year:${state.selectedTargetword.networks[0].year}){
                 id
                 year
@@ -121,13 +115,13 @@ const store = new Vuex.Store({
           }`
                 };
                 const response = await axios.post('https://dylen-ego-network-service.acdh-dev.oeaw.ac.at/graphql', graphqlQuery);
-                const networkID = state.selectedTargetword.id + state.selectedTargetword.networks[0].year
+                const networkID = state.selectedTargetword.id+state.selectedTargetword.networks[0].year
                 let network = response.data.data.getNetwork;
                 network.id = networkID
                 network.corpus = state.selectedCorpus.name
                 network.source = state.selectedSubcorpus.name
                 network.text = state.selectedTargetword.text
-                this.commit('main/addEgoNetwork', network);
+                this.commit('addEgoNetwork', network);
             } catch (error) {
                 console.log(error);
             }
