300c2dffccc35d7a806a2182a66f2c50842a3b02
Can <canyil@gmail.com>
2021-03-02 16:10:51 +0100

    correct sauto url in properties. now it worsk with dev properties

--- src/properties/properties.js
+++ src/properties/properties.js
@@ -1,4 +1,4 @@
 export default {
-  sautoURI: "ws://sauto.acdh.oeaw.ac.at/app",
+  sautoURI: "wss://sauto.acdh.oeaw.ac.at/app",
   graphqlEndpoint: "https://dylen-ego-network-service.acdh.oeaw.ac.at/graphql",
 };
