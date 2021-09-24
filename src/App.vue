56ab33843d6710d8af9e70a683c514a1266ae1e0
Can <canyil@gmail.com>
2021-09-24 14:14:55 +0200

    try different formattings

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,59 @@
 <template>
   <div
-      id="app"
-      ref="app"
-      style="margin-top: 0; height: 95%"
-      @mousemove="mouseMove"
-      @click="mouseClick"
-      @wheel="scroll"
-      @keypress="keyPress"
+    id="app"
+    ref="app"
+    style="margin-top: 0; height: 95%"
+    @mousemove="mouseMove"
+    @click="mouseClick"
+    @wheel="scroll"
+    @keypress="keyPress"
   >
-    <div
-        ref="main"
-        class="main full"
-    >
-      <b-container
-          class="pt-1 pb-5 h-100"
-          fluid
-      >
+    <div ref="main" class="main full">
+      <b-container class="pt-1 pb-5 h-100" fluid>
         <b-row>
           <b-col>
-            <top-navigation
-                class="mb-20"
-                style="min-height: 72px"
-            >
+            <top-navigation class="mb-20" style="min-height: 72px">
             </top-navigation>
           </b-col>
         </b-row>
-        <b-row
-            xl="12"
-            class="h-100"
-        >
-          <b-col
-              class="h-100"
-              xl="12"
-          >
-            <div
-                class="h-100"
-                @mouseover="mouseOver"
-                data-sauto-id="results"
-            >
-              <Results/>
+        <b-row xl="12" class="h-100">
+          <b-col class="h-100" xl="12">
+            <div class="h-100" @mouseover="mouseOver" data-sauto-id="results">
+              <Results />
             </div>
           </b-col>
         </b-row>
-        <Modal/>
+        <Modal />
       </b-container>
     </div>
 
     <div
-        ref="sidebar"
-        class="sidebar collapsed"
-        @mouseover="mouseOver"
-        data-sauto-id="sidebar"
+      ref="sidebar"
+      class="sidebar collapsed"
+      @mouseover="mouseOver"
+      data-sauto-id="sidebar"
     >
-      <div
-          class="px-4 py-3"
-          style="width: 300px"
-      >
+      <div class="px-4 py-3" style="width: 300px">
         <b-button
-            type="button"
-            class="close"
-            aria-label="Close"
-            variant="light"
-            @click="toggleSideBar"
-            data-sauto-id="sidebar-close-button"
+          type="button"
+          class="close"
+          aria-label="Close"
+          variant="light"
+          @click="toggleSideBar"
+          data-sauto-id="sidebar-close-button"
         >
           <span aria-hidden="true">×</span>
         </b-button>
         <h4 class="mt-3">Part-of-speech colors</h4>
-        <b-row
-            v-for="key in Object.keys(posColors)"
-            :key="key"
-            class="my-3"
-        >
-          <b-col>{{
-              key[0].toUpperCase() + key.slice(1).replace('_', ' ')
-            }}
+        <b-row v-for="key in Object.keys(posColors)" :key="key" class="my-3">
+          <b-col
+            >{{ key[0].toUpperCase() + key.slice(1).replace('_', ' ') }}
           </b-col>
           <b-col>
             <b-form-input
-                :data-sauto-id="'color-option-'+key"
-                type="color"
-                v-model="posColors[key]"
+              :data-sauto-id="'color-option-' + key"
+              type="color"
+              v-model="posColors[key]"
             ></b-form-input>
           </b-col>
         </b-row>
@@ -89,10 +62,10 @@
           <b-col>Font size</b-col>
           <b-col>
             <b-form-input
-                data-sauto-id="font-option"
-                type="number"
-                v-model="labelOptions.fontSize"
-                :number="true"
+              data-sauto-id="font-option"
+              type="number"
+              v-model="labelOptions.fontSize"
+              :number="true"
             >
             </b-form-input>
           </b-col>
@@ -100,14 +73,19 @@
         <b-row>
           <b-col>Bold</b-col>
           <b-col>
-            <b-form-checkbox data-sauto-id="bold-checkbox-option" v-model="labelOptions.bold"></b-form-checkbox>
+            <b-form-checkbox
+              data-sauto-id="bold-checkbox-option"
+              v-model="labelOptions.bold"
+            ></b-form-checkbox>
           </b-col>
         </b-row>
         <b-row>
           <b-col>White label background</b-col>
           <b-col>
-            <b-form-checkbox data-sauto-id="white-label-checkbox-option"
-                             v-model="labelOptions.background"></b-form-checkbox>
+            <b-form-checkbox
+              data-sauto-id="white-label-checkbox-option"
+              v-model="labelOptions.background"
+            ></b-form-checkbox>
           </b-col>
         </b-row>
         <h4 class="mt-3">Link options</h4>
@@ -115,38 +93,43 @@
           <b-col>Opacity: {{ linkOptions.opacity }}</b-col>
           <b-col>
             <b-form-input
-                data-sauto-id="opacity-slider-option"
-                type="range"
-                v-model="linkOptions.opacity"
-                :number="true"
-                :min="0"
-                :max="1"
-                :step="0.1"
+              data-sauto-id="opacity-slider-option"
+              type="range"
+              v-model="linkOptions.opacity"
+              :number="true"
+              :min="0"
+              :max="1"
+              :step="0.1"
             >
             </b-form-input>
           </b-col>
         </b-row>
         <h4 class="mt-3">Table options</h4>
         <b-row>
-          <b-col>Number of digits to display: {{ tableOptions.digits > 10 ? 'all' : tableOptions.digits }}</b-col>
+          <b-col
+            >Number of digits to display:
+            {{ tableOptions.digits > 10 ? 'all' : tableOptions.digits }}</b-col
+          >
           <b-col>
             <b-form-input
-                data-sauto-id="digits-slider-option"
-                type="range"
-                v-model="tableOptions.digits"
-                :number="true"
-                :min="0"
-                :max="11"
-                :step="1"
+              data-sauto-id="digits-slider-option"
+              type="range"
+              v-model="tableOptions.digits"
+              :number="true"
+              :min="0"
+              :max="11"
+              :step="1"
             >
             </b-form-input>
-
           </b-col>
         </b-row>
         <b-row>
           <b-col>Show selected words on top</b-col>
           <b-col>
-            <b-check data-sauto-id="selected-words-top-checkbox-option" v-model="tableOptions.selectedOnTop">
+            <b-check
+              data-sauto-id="selected-words-top-checkbox-option"
+              v-model="tableOptions.selectedOnTop"
+            >
             </b-check>
           </b-col>
         </b-row>
--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -1,33 +1,23 @@
 <template>
-  <b-row
-      xl="12"
-      class="top-nav"
-      align-h="center"
-      align-v="stretch"
-  >
-    <b-col
-        xl="1"
-        align-self="center"
-    >
+  <b-row xl="12" class="top-nav" align-h="center" align-v="stretch">
+    <b-col xl="1" align-self="center">
       <h2><b>DYLEN</b></h2>
     </b-col>
     <b-col
-        class="pb-1"
-        xl="5"
-        align-self="stretch"
+      @mouseover="mouseOver"
+      data-sauto-id="search-form-1"
+      class="pb-1"
+      xl="5"
+      align-self="stretch"
     >
-      <b-row
-          class="h-100"
-          align-h="center"
-          align-v="stretch"
-      >
+      <b-row class="h-100" align-h="center" align-v="stretch">
         <b-col>
           <b-row>
             <b-col>
               <search-form
-                  :with-labels="false"
-                  :pane="'pane' + 1"
-                  :is-sidebar="false"
+                :with-labels="false"
+                :pane="'pane' + 1"
+                :is-sidebar="false"
               >
               </search-form>
             </b-col>
@@ -36,44 +26,41 @@
       </b-row>
     </b-col>
     <b-col
-        class="pb-1"
-        xl="5"
-        align-self="stretch"
+      @mouseover="mouseOver"
+      data-sauto-id="search-form-2"
+      class="pb-1"
+      xl="5"
+      align-self="stretch"
     >
-      <b-row
-          class="h-100"
-          align-v="center"
-      >
+      <b-row class="h-100" align-v="center">
         <b-col v-if="!secondForm">
           <b-button
-              v-if="firstForm && !secondForm"
-              size="sm"
-              variant="secondary"
-              v-on:click="queryButtonClicked(2)"
+            v-if="firstForm && !secondForm"
+            size="sm"
+            variant="secondary"
+            v-on:click="queryButtonClicked(2)"
           >
             <b>+</b>
           </b-button>
         </b-col>
         <b-col v-else>
           <search-form
-              :with-labels="false"
-              :pane="'pane' + 2"
-              :is-sidebar="false"
+            :with-labels="false"
+            :pane="'pane' + 2"
+            :is-sidebar="false"
           >
           </search-form>
         </b-col>
         <b-col xl="1"></b-col>
       </b-row>
     </b-col>
-    <b-col
-        xl="1"
-        align-self="center"
-    >
+    <b-col xl="1" align-self="center">
       <b-button
-          data-sauto-id="settings-button"
-          class="float-right"
-          @click="toggleSideBar"
-      > Settings
+        data-sauto-id="settings-button"
+        class="float-right"
+        @click="toggleSideBar"
+      >
+        Settings
       </b-button>
     </b-col>
   </b-row>
