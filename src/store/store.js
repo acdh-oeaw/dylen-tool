f0493036dda74f02d180657631fe78f036ce9f69
Can <canyil@gmail.com>
2021-09-27 16:48:49 +0200

    fix merge conflicts

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,13 @@ import Vuex from 'vuex';
 import axios from 'axios';
 import props from '../properties/propertiesLoader';
 import {
-    allAvailableCorporaQuery,
-    getNetworkQuery,
-    getSoucesByCorpusQuery,
-    getNetworksByCorpusAndSource,
-    getAutocompleteSuggestionsQuery
-} from "@/queries/queries";
-import {ExportToCsv} from 'export-to-csv';
+  allAvailableCorporaQuery,
+  getNetworkQuery,
+  getSoucesByCorpusQuery,
+  getNetworksByCorpusAndSource,
+  getAutocompleteSuggestionsQuery
+} from '@/queries/queries';
+import { ExportToCsv } from 'export-to-csv';
 
 Vue.prototype.axios = axios;
 Vue.use(Vuex);
@@ -60,256 +60,49 @@ const mainModule = {
       await dispatch('loadTargetWords', 'pane1');
       await dispatch('loadTargetWords', 'pane2');
 
-const mainModule = {
-    actions: {
-        async initCorpora({dispatch}) {
-            const response = await axios.post(graphqlEndpoint, allAvailableCorporaQuery);
-            const corporaPayload = {
-                corpora: response.data.data.allAvailableCorpora
-            }
-            this.commit('main/loadCorpora', corporaPayload);
-            await dispatch("loadSources")
-        },
-        async loadSources({dispatch, state}) {
-            for (const corpus of state.availableCorpora) {
-                const sourceResponse = await axios.post(graphqlEndpoint, getSoucesByCorpusQuery(corpus))
-                const sourcesPayload = {
-                    corpus: corpus,
-                    sources: sourceResponse.data.data.getSourcesByCorpus
-                }
-                this.commit('main/loadSourcesOfCorpus', sourcesPayload)
-            }
-            this.commit("main/selectInitValues", {pane: "pane1"})
-            this.commit("main/selectInitValues", {pane: "pane2"})
-
-            await dispatch("loadTargetWords", "pane1")
-            await dispatch("loadTargetWords", "pane2")
-
-        },
-        async loadTargetWords({state}, pane) {
-            const targetwordsResponse = await axios.post(graphqlEndpoint,
-                getNetworksByCorpusAndSource(state[pane].selectedCorpus, state[pane].selectedSubcorpus, 0, 20))
-            const payload = {
-                pane: pane,
-                targetWords: targetwordsResponse.data.data.getNetworksByCorpusAndSource.targetWords
-            }
-            this.commit("main/loadTargetwordsOfCorpusAndSource", payload)
-            this.commit("main/selectInitTargetWord", {pane: pane})
-        },
-        async loadAvailableCorpora({dispatch}) {
-            try {
-                await dispatch("initCorpora")
-                logger.log('Query parameters loaded successfully.')
-            } catch (error) {
-                logger.error(error);
-            }
-        },
-        async loadEgoNetwork({state}, pane) {
-            function assignValuesFromState(network, networkID) {
-                network.id = networkID
-                network.targetWordId = state[pane].selectedTargetword.id
-                network.corpus = state[pane].selectedCorpus
-                network.subcorpus = state[pane].selectedSubcorpus
-                network.text = state[pane].selectedTargetword.text
-                network.possibleYears = state[pane].selectedTargetword.networks.map(n => n.year)
-            }
-
-            let year_param = state[pane].selectedYear ? state[pane].selectedYear.year : state[pane].selectedTargetword.networks[0].year
-
-            try {
-                const response = await axios.post(graphqlEndpoint,
-                    getNetworkQuery(state[pane].selectedTargetword.id, year_param));
-                const networkID = state[pane].selectedTargetword.id + state[pane].selectedYear.year
-                let network = response.data.data.getNetwork;
-
-                assignValuesFromState(network, networkID);
-
-                const payload = {
-                    pane: pane,
-                    network: network
-                }
-
-                this.commit('main/addEgoNetwork', payload);
-                logger.log('Ego Network loaded successfully.')
-
-            } catch (error) {
-                logger.error(error);
-            }
-        },
-        async loadUpdatedEgoNetwork(state, {network: oldNetwork, pane: pane}) {
-            try {
-
-                const response = await axios.post(graphqlEndpoint,
-                    getNetworkQuery(oldNetwork.targetWordId, oldNetwork.year));
-
-                const networkID = oldNetwork.targetWordId + oldNetwork.year
-                let updatedNetwork = response.data.data.getNetwork;
-
-                updatedNetwork.id = networkID
-                updatedNetwork.targetWordId = oldNetwork.targetWordId
-                updatedNetwork.corpus = oldNetwork.corpus
-                updatedNetwork.subcorpus = oldNetwork.subcorpus
-                updatedNetwork.text = oldNetwork.text
-                updatedNetwork.possibleYears = oldNetwork.possibleYears
-                logger.log('Ego Network %s updated successfully.', networkID)
-
-                this.commit('main/updateEgoNetwork', {networkObj: updatedNetwork, pane: pane});
-            } catch (error) {
-                logger.error(error);
-            }
-        },
-        async loadAutocompleteSuggestions({state}, {pane}) {
-            const targetwordsResponse = await axios.post(graphqlEndpoint,
-                getAutocompleteSuggestionsQuery(state[pane].selectedCorpus, state[pane].selectedSubcorpus, state[pane].searchTerm));
-            this.commit("main/setAutocompleteSuggestions", {suggestions: targetwordsResponse.data.data.getAutocompleteSuggestions, pane: pane});
-            
-        },
-        async downloadMetricsAsCSV({state}, nodes) {
-            let data = nodesToJSON(state, nodes);
-            const options = {
-                filename: "DYLEN_Export",
-                fieldSeparator: ',',
-                quoteStrings: '"',
-                decimalSeparator: '.',
-                showLabels: true,
-                showTitle: false,
-                useTextFile: false,
-                useBom: true,
-                useKeysAsHeaders: true,
-            };
-
-            const csvExporter = new ExportToCsv(options);
-            csvExporter.generateCsv(data);
-        },
-        async downloadMetricsAsJSON({state}, nodes) {
-            let exportName = "DYLEN_Export";
-            let data = nodesToJSON(state, nodes);
-            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
-            var downloadAnchorNode = document.createElement('a');
-            downloadAnchorNode.setAttribute("href", dataStr);
-            downloadAnchorNode.setAttribute("download", exportName + ".json");
-            document.body.appendChild(downloadAnchorNode); // required for firefox
-            downloadAnchorNode.click();
-            downloadAnchorNode.remove();
-        }
     },
-    namespaced: true,
-    state: {
-        selectionColors: ["#1E88E5", "#D81B60"],
-        availableCorpora: [],
-        availableSourcesByCorpus: {},
-        availableTargetwordsByCorpusAndSource: {},
-        pane1: {
-            selectedCorpus: {id: '', name: '', sources: []},
-            selectedSubcorpus: {id: '', name: '', targetWords: []},
-            selectedTargetword: {id: '', text: ''},
-            selectedYear: null,
-            selectedNetwork: null,
-            searchTerm: null,
-            autocompleteSuggestions: []
-        },
-        pane2: {
-            selectedCorpus: {id: '', name: '', sources: []},
-            selectedSubcorpus: {id: '', name: '', targetWords: []},
-            selectedTargetword: {id: '', text: ''},
-            selectedYear: null,
-            selectedNetwork: null,
-            searchTerm: null,
-            autocompleteSuggestions: []
-        },
-        nodeMetrics: {
-            selectedNodes: []
-        },
-        posColors: {
-            noun: '#aa0000',
-            verb: '#000088',
-            adjective: '#f18e04',
-            proper_noun: '#000000',
-        },
-        labelOptions: {
-            fontSize: 12,
-            bold: false,
-            background: true
-        },
-        linkOptions: {
-            opacity: 0.25
-        },
-        tableOptions: {
-            digits: 3,
-            selectedOnTop: true
-        }
+    async loadTargetWords({ state }, pane) {
+      const targetwordsResponse = await axios.post(graphqlEndpoint,
+        getNetworksByCorpusAndSource(state[pane].selectedCorpus, state[pane].selectedSubcorpus, 0, 20));
+      const payload = {
+        pane: pane,
+        targetWords: targetwordsResponse.data.data.getNetworksByCorpusAndSource.targetWords
+      };
+      this.commit('main/loadTargetwordsOfCorpusAndSource', payload);
+      this.commit('main/selectInitTargetWord', { pane: pane });
     },
-    mutations: {
-        loadCorpora(state, payload) {
-            state.availableCorpora = payload.corpora;
-        },
-        loadSourcesOfCorpus(state, payload) {
-            Vue.set(state.availableSourcesByCorpus, payload.corpus, payload.sources);
-        },
-        loadTargetwordsOfCorpusAndSource(state, payload) {
-            if (!state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus]) {
-                Vue.set(state.availableTargetwordsByCorpusAndSource, state[payload.pane].selectedCorpus, {});
-            }
-            Vue.set(state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus], state[payload.pane].selectedSubcorpus, payload.targetWords);
-        },
-        selectInitValues(state, payload) {
-            state[payload.pane].selectedCorpus = state.availableCorpora[0];
-            state[payload.pane].selectedSubcorpus = state.availableSourcesByCorpus[state[payload.pane].selectedCorpus][0];
-        },
-        selectInitTargetWord(state, payload) {
-            state[payload.pane].selectedTargetword = state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus][state[payload.pane].selectedSubcorpus][0]
-            state[payload.pane].selectedYear = state[payload.pane].selectedTargetword.networks[0]
-        },
-        changeSelectedCorpus(state, payload) {
-            if (payload.corpus) {
-                state[payload.pane].selectedCorpus = payload.corpus;
-            } else {
-                state[payload.pane].selectedCorpus = state[payload.pane].availableCorpora()[0];
-            }
-        },
-        changeSelectedSubcorpus(state, payload) {
-            state[payload.pane].selectedSubcorpus = payload.subcorpus? payload.subcorpus : state.availableSourcesByCorpus[state[payload.pane].selectedCorpus][0];
-        },
-        changeSelectedTargetword(state, payload) {
-            if (payload.targetword) {
-                state[payload.pane].selectedTargetword = payload.targetword;
-            } else {
-                state[payload.pane].selectedTargetword = state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus][state[payload.pane].selectedSubcorpus][0]
-            }
-            this.commit('main/changeSelectedYear', {pane: payload.pane});
-        },
-        changeSearchTerm(state, payload) {
-            if (payload.targetword) {
-                state[payload.pane].searchTerm = payload.targetword;
-            } else {
-                state[payload.pane].searchTerm = state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus][state[payload.pane].selectedSubcorpus][0]
-            }
-            this.dispatch('main/loadAutocompleteSuggestions', {pane: payload.pane});
-        },
-        changeSelectedYear(state, payload) {
-            if (payload.year) {
-                state[payload.pane].selectedYear = payload.year;
-            } else {
-                state[payload.pane].selectedYear = state[payload.pane].selectedTargetword.networks[0]
-            }
-        },
-        addSelectedNodeForNodeMetrics(state, payload) {
-            state["nodeMetrics"].selectedNodes.push(payload)
-        },
-        removeSelectedNodeForNodeMetrics(state, payload) {
-            let payloadIndex = state["nodeMetrics"].selectedNodes.indexOf(payload);
-            state["nodeMetrics"].selectedNodes.splice(payloadIndex, 1);
-        },
-        addEgoNetwork(state, payload) {
-            state[payload['pane']].selectedNetwork = payload.network
-        },
-        updateEgoNetwork(state, payload) {
-            state[payload.pane].selectedNetwork = payload.networkObj
-            logger.log("Updated Ego Network for pane " + payload.pane)
-        },
-        setAutocompleteSuggestions(state, payload) {
-            state[payload.pane].autocompleteSuggestions = payload.suggestions;
-        }
+    async loadAvailableCorpora({ dispatch }) {
+      try {
+        await dispatch('initCorpora');
+        logger.log('Query parameters loaded successfully.');
+      } catch (error) {
+        logger.error(error);
+      }
+    },
+    async loadEgoNetwork({ state }, pane) {
+      function assignValuesFromState(network, networkID) {
+        network.id = networkID;
+        network.targetWordId = state[pane].selectedTargetword.id;
+        network.corpus = state[pane].selectedCorpus;
+        network.subcorpus = state[pane].selectedSubcorpus;
+        network.text = state[pane].selectedTargetword.text;
+        network.possibleYears = state[pane].selectedTargetword.networks.map(n => n.year);
+      }
+
+      let year_param = state[pane].selectedYear ? state[pane].selectedYear.year : state[pane].selectedTargetword.networks[0].year;
+
+      try {
+        const response = await axios.post(graphqlEndpoint,
+          getNetworkQuery(state[pane].selectedTargetword.id, year_param));
+        const networkID = state[pane].selectedTargetword.id + state[pane].selectedYear.year;
+        let network = response.data.data.getNetwork;
+
+        assignValuesFromState(network, networkID);
+
+        const payload = {
+          pane: pane,
+          network: network
+        };
 
         this.commit('main/addEgoNetwork', payload);
         logger.log('Ego Network loaded successfully.');
@@ -339,6 +132,15 @@ const mainModule = {
       } catch (error) {
         logger.error(error);
       }
+    },
+    async loadAutocompleteSuggestions({ state }, { pane }) {
+      const targetwordsResponse = await axios.post(graphqlEndpoint,
+        getAutocompleteSuggestionsQuery(state[pane].selectedCorpus, state[pane].selectedSubcorpus, state[pane].searchTerm));
+      this.commit('main/setAutocompleteSuggestions', {
+        suggestions: targetwordsResponse.data.data.getAutocompleteSuggestions,
+        pane: pane
+      });
+
     },
     async downloadMetricsAsCSV({ state }, nodes) {
       let data = nodesToJSON(state, nodes);
@@ -380,14 +182,18 @@ const mainModule = {
       selectedSubcorpus: { id: '', name: '', targetWords: [] },
       selectedTargetword: { id: '', text: '' },
       selectedYear: null,
-      selectedNetwork: null
+      selectedNetwork: null,
+      searchTerm: null,
+      autocompleteSuggestions: []
     },
     pane2: {
       selectedCorpus: { id: '', name: '', sources: [] },
       selectedSubcorpus: { id: '', name: '', targetWords: [] },
       selectedTargetword: { id: '', text: '' },
       selectedYear: null,
-      selectedNetwork: null
+      selectedNetwork: null,
+      searchTerm: null,
+      autocompleteSuggestions: []
     },
     nodeMetrics: {
       selectedNodes: []
@@ -450,30 +256,37 @@ const mainModule = {
       }
       this.commit('main/changeSelectedYear', { pane: payload.pane });
     },
-    getters: {
-        selectionColors: (state) => state.selectionColors,
-        availableCorpora: (state) => state.availableCorpora,
-        selectedCorpus: (state) => (pane) => state[pane].selectedCorpus,
-        availableSourcesByCorpus: (state) => (selectedCorpus) => state['availableSourcesByCorpus'][selectedCorpus],
-        selectedSubcorpus: (state) => (pane) => state[pane].selectedSubcorpus,
-        targetwordsOfCorpusAndSource: (state) => (corpus, selectedSubcorpus) => {
-            if (!state.availableTargetwordsByCorpusAndSource[corpus] || !state.availableTargetwordsByCorpusAndSource[corpus][selectedSubcorpus])
-                return []
-            return state.availableTargetwordsByCorpusAndSource[corpus][selectedSubcorpus]
-        },
-        selectedTargetword: (state) => (pane) => state[pane].selectedTargetword,
-        selectedYear: (state) => (pane) => state[pane].selectedYear,
-        searchTerm: (state) => (pane) => state[pane].searchTerm,
-        autocompleteSuggestions: (state) => (pane) => state[pane].autocompleteSuggestions,
-        getPane: (state) => (pane) => state[pane],
-        selectedNodesForMetrics: (state) => state["nodeMetrics"].selectedNodes,
-        posColors: (state) => state.posColors,
-        labelOptions: (state) => state.labelOptions,
-        linkOptions: (state) => state.linkOptions,
-        tableOptions: (state) => state.tableOptions,
+    changeSearchTerm(state, payload) {
+      if (payload.targetword) {
+        state[payload.pane].searchTerm = payload.targetword;
+      } else {
+        state[payload.pane].searchTerm = state.availableTargetwordsByCorpusAndSource[state[payload.pane].selectedCorpus][state[payload.pane].selectedSubcorpus][0];
+      }
+      this.dispatch('main/loadAutocompleteSuggestions', { pane: payload.pane });
     },
-    setPosColor({state}, posTag, colorCode) {
-        state.posColors[posTag] = colorCode;
+    changeSelectedYear(state, payload) {
+      if (payload.year) {
+        state[payload.pane].selectedYear = payload.year;
+      } else {
+        state[payload.pane].selectedYear = state[payload.pane].selectedTargetword.networks[0];
+      }
+    },
+    addSelectedNodeForNodeMetrics(state, payload) {
+      state['nodeMetrics'].selectedNodes.push(payload);
+    },
+    removeSelectedNodeForNodeMetrics(state, payload) {
+      let payloadIndex = state['nodeMetrics'].selectedNodes.indexOf(payload);
+      state['nodeMetrics'].selectedNodes.splice(payloadIndex, 1);
+    },
+    addEgoNetwork(state, payload) {
+      state[payload['pane']].selectedNetwork = payload.network;
+    },
+    updateEgoNetwork(state, payload) {
+      state[payload.pane].selectedNetwork = payload.networkObj;
+      logger.log('Updated Ego Network for pane ' + payload.pane);
+    },
+    setAutocompleteSuggestions(state, payload) {
+      state[payload.pane].autocompleteSuggestions = payload.suggestions;
     }
 
   },
@@ -490,6 +303,8 @@ const mainModule = {
     },
     selectedTargetword: (state) => (pane) => state[pane].selectedTargetword,
     selectedYear: (state) => (pane) => state[pane].selectedYear,
+    searchTerm: (state) => (pane) => state[pane].searchTerm,
+    autocompleteSuggestions: (state) => (pane) => state[pane].autocompleteSuggestions,
     getPane: (state) => (pane) => state[pane],
     selectedNodesForMetrics: (state) => state['nodeMetrics'].selectedNodes,
     posColors: (state) => state.posColors,
