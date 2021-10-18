b41ee2756cea846125eb0ca337cd7178985190b7
Katharina Wünsche <katharina_wunsche@yahoo.de>
2021-10-18 17:10:18 +0200

    Added indicator on mouseover

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -26,6 +26,35 @@
         :stroke="colors[idx]"
       />
     </g>
+    <rect
+      fill="none"
+      style="pointer-events: all;"
+      :width="options.size.w"
+      :height="options.size.h"
+      @mouseover="mouseover"
+      @mousemove="mousemove"
+      @mouseout="mouseout"
+    >
+    </rect>
+    <g
+      v-for='(_, idx) in data'
+      :key="`indicator${idx}`"
+    >
+      <circle
+        :ref="`focus${idx}`"
+        fill="none"
+        :stroke="colors[idx]"
+        r="7"
+        style="opacity: 0;"
+      ></circle>
+      <text
+        :ref="`focusText${idx}`"
+        text-anchor="start"
+        alignment-baseline="middle"
+        style="opacity: 0;"
+      ></text>
+    </g>
+
   </svg>
 </template>
 <script>
@@ -39,7 +68,7 @@ export default {
       hoverNodes: [],
       svgPadding: {
         top: 20,
-        right: 5,
+        right: 15,
         bottom: 50,
         left: 10
       }
@@ -68,13 +97,13 @@ export default {
     scaleX() {
       return d3
         .scalePoint()
-        .domain([...Array(this.data[0].length).keys()].map((val) => val + 1997))
+        .domain([...Array(this.data[0].length).keys()].map((val) => val))
         .range([this.svgPadding.left, this.chartSize[0]]);
     },
     lineGenerator() {
       return d3
         .line()
-        .x((_, idx) => this.scaleX(idx + 1997))
+        .x((_, idx) => this.scaleX(idx))
         .y((d) => this.scaleY(d))
         .curve(d3.curveCardinal.tension(0.5));
     }
@@ -96,6 +125,47 @@ export default {
     onMouseLeave(e, node) {
       if (this.hoverNodes.indexOf(node) >= 0)
         this.hoverNodes.splice(this.hoverNodes.indexOf(node), 1);
+    },
+    mouseover() {
+      this.data.forEach((_, idx) => {
+        d3.select(this.$refs[`focus${idx}`][0]).style('opacity', 1);
+        d3.select(this.$refs[`focusText${idx}`][0]).style('opacity', 1);
+      });
+    },
+    mousemove(event) {
+      var xPos = d3.pointer(event)[0];
+      var domain = this.scaleX.domain();
+      var range = this.scaleX.range();
+      var rangePoints = d3.range(range[0], range[1], this.scaleX.step());
+      var x0 = domain[d3.bisect(rangePoints, xPos)];
+      if (x0 != undefined && x0 >= 0)
+        this.data.forEach((_, idx) => {
+          let selectedData = this.data[idx][x0];
+          d3.select(this.$refs[`focus${idx}`][0])
+            .attr('cx', this.scaleX(x0))
+            .attr('cy', this.scaleY(selectedData));
+          d3.select(this.$refs[`focusText${idx}`][0])
+            .html(
+              'year: ' +
+                (1996 + x0) +
+                ' - ' +
+                'value: ' +
+                Math.round(selectedData * 100) / 100
+            )
+            .attr('x', this.scaleX(x0) + 15)
+            .attr('y', this.scaleY(selectedData))
+            .attr('text-anchor', 'start');
+          if (x0 > this.data[idx].length / 2)
+            d3.select(this.$refs[`focusText${idx}`][0])
+              .attr('text-anchor', 'end')
+              .attr('x', this.scaleX(x0) - 15);
+        });
+    },
+    mouseout() {
+      this.data.forEach((_, idx) => {
+        d3.select(this.$refs[`focus${idx}`][0]).style('opacity', 1);
+        d3.select(this.$refs[`focusText${idx}`][0]).style('opacity', 1);
+      });
     }
   },
   directives: {
@@ -103,11 +173,16 @@ export default {
       const axis = binding.arg;
       const axisMethod = { x: 'axisBottom', y: 'axisLeft' }[axis];
       const tickFilter = { x: (d) => d % 2 == 0, y: false }[axis];
+      const tickFormat = { x: (d) => d + 1996, y: (d) => d }[axis];
       const methodArg = binding.value;
       d3.select(el).call(
-        d3[axisMethod](methodArg).tickValues(
-          tickFilter ? methodArg.domain().filter(tickFilter) : methodArg.ticks()
-        )
+        d3[axisMethod](methodArg)
+          .tickValues(
+            tickFilter
+              ? methodArg.domain().filter(tickFilter)
+              : methodArg.ticks()
+          )
+          .tickFormat(tickFormat)
       );
     }
   }
