077d0b92a29f0223a79b85e7767c5fa9ff161b28
Seungbin Yim <liam21c@gmail.com>
2021-11-02 13:23:35 +0100

    Change button horizontal alignment

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -87,31 +87,35 @@
           </datalist>
         </b-form-group>
       </b-col>
-
-      <b-col
-        xl='6'
-      >
-        <b-button
-            size='sm'
-            type='submit'
-            variant='secondary'
-            :data-sauto-id='"queryButton-"+this.pane'
-            :disabled='!queryButtonActive'
-            @click='setShowInfo'
-        >
-          Query
-        </b-button>
-        <b-button
-            size='sm'
-            variant='secondary'
-            :data-sauto-id='"queryButton-"+this.pane'
-            @click='initialize'
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-reboot" viewBox="0 0 16 16">
-            <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z"/>
-            <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z"/>
-          </svg>
-        </b-button>
+      <b-col xl='2'></b-col>
+      <b-col xl='4'>
+        <b-row align-h='end'>
+          <b-col xl='12' class='align-end'>
+            <b-button
+                class='visualize-button'
+                size='sm'
+                type='submit'
+                variant='secondary'
+                :data-sauto-id='"queryButton-"+this.pane'
+                :disabled='!queryButtonActive'
+                @click='setShowInfo'
+            >
+              Visualize
+            </b-button>
+            <b-button
+                class='reset-button'
+                size='sm'
+                variant='secondary'
+                :data-sauto-id='"queryButton-"+this.pane'
+                @click='initialize'
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-reboot" viewBox="0 0 16 16">
+                <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z"/>
+                <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z"/>
+              </svg>
+            </b-button>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-form>
@@ -312,5 +316,18 @@ export default {
   font-size: 0.7rem;
   line-height: 1.5;
   border-radius: 0.2rem;
+}
+.visualize-button {
+  margin-right: 0.5em
+}
+.align-end {
+  text-align:end;
+}
+.reset-button {
+  border: none;
+  color: red;
+  background-color: white;
+  border-color: white;
+
 }
 </style>
