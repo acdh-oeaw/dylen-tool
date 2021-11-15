073fcd011a2986e11ddf58fb08cf35962b8db971
Seungbin Yim <liam21c@gmail.com>
2021-11-15 12:13:22 +0100

    Add time series metrics information

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -135,28 +135,70 @@
             <b-tab title='Time Series Analysis'>
               <b-card-text align='left'>
                 <p><b>Time series analysis</b> allows us to track the change over time. <br>
-                Each graph indicates the diachronic development of a given target word according to the frequency change
-                or to one of the similarity measures (all measures are introduced later).<br>
+                  Each graph indicates the diachronic development of a given target word according to the frequency
+                  change
+                  or to one of the similarity measures (all measures are introduced later).<br>
                 </p>
                 <p>
-                Each measure provides a single score by comparing data from two time periods, and thus produces three
-                time series graphs depending on which time periods and in what order are compared. <br>
-                The first perspective is to compare each subsequent year to the previous one, second - each subsequent year to the first year,
-                and third - each subsequent year to the last year. <br>
-                Only the similarity score based on network embeddings
-                doesn't follow the procedure described above. Instead of providing point-wise comparison, this measure
-                adopts a cumulative strategy. The idea behind the cumulative approach is that semantic similarity is
-                defined not by a single previous year, but by all previous time periods.
+                  Each measure provides a single score by comparing data from two time periods, and thus produces three
+                  time series graphs depending on which time periods and in what order are compared. <br>
+                  The first perspective is to compare each subsequent year to the previous one, second - each subsequent
+                  year to the first year,
+                  and third - each subsequent year to the last year. <br>
+                  Only the similarity score based on network embeddings
+                  doesn't follow the procedure described above. Instead of providing point-wise comparison, this measure
+                  adopts a cumulative strategy. The idea behind the cumulative approach is that semantic similarity is
+                  defined not by a single previous year, but by all previous time periods.
                 </p>
                 <p>
-                All similarity scores are based on the network representation of a target word in different time
-                periods. Some of the measures make use only of network nodes lists (not the network itself), however the
-                set of nodes is also influenced by the network structure. To interpret similarity measures one can
-                assume that a high value suggests that the context of a word has not changed much, while a low value
-                implies that the meaning of a word might have changed from one time point to another. Only Frobenius
-                similarity differs in interpretation of its scores: high values indicate the most changed network
-                structure of a word in corresponding time periods.
+                  All similarity scores are based on the network representation of a target word in different time
+                  periods. Some of the measures make use only of network nodes lists (not the network itself), however
+                  the
+                  set of nodes is also influenced by the network structure. To interpret similarity measures one can
+                  assume that a high value suggests that the context of a word has not changed much, while a low value
+                  implies that the meaning of a word might have changed from one time point to another. Only Frobenius
+                  similarity differs in interpretation of its scores: high values indicate the most changed network
+                  structure of a word in corresponding time periods.
                 </p>
+                <ul>
+                  <li>
+                    <b>Frequency difference:</b> the measure shows the absolute difference of normalized frequency
+                    values in two years.
+                  </li>
+                  <li>
+                    <b>Jaccard similarity:</b> Jaccard index [4] is applied to measure the similarity between node sets
+                    of target word networks in two years. It is defined as the size of the intersection divided by the
+                    size of the union of the given sets. The score ranges from 0 to 1.
+                  </li>
+                  <li>
+                    <b>RankDCG similarity:</b> Rank discounted cumulative gain score is also applied to measure the
+                    similarity
+                    between node sets of target word networks in two years. However, unlike Jaccard similarity, it takes
+                    into account semantic closeness of nodes to a target word, and it represents an improved version of
+                    the normalized discounted cumulative gain metric described in [5]. The score ranges from 0 to 1.
+                  </li>
+                  <li>
+                    <b>Local neighborhood similarity:</b> this measure is often used for semantic change detection, it
+                    was
+                    introduced by [3]. Two second-order vectors are created corresponding to two time periods. Their
+                    length equals the size of nodes union, and cosine similarities between a target word and each node
+                    provide scores of the vectors. Then, cosine similarity is computed between these vectors. The score
+                    ranges from 0 to 1.
+                  </li>
+                  <li>
+                    <b>Frobenius similarity:</b> this measure is computed by taking the Frobenius norm [1] of a matrix
+                    obtained
+                    as a difference between adjacency matrices of target word networks in two years. Adjacency matrices
+                    are pre-processed to represent the union of nodes of two time slices. The score doesn't have a fixed
+                    range.
+                  </li>
+                  <li>
+                    <b>Network embeddings similarity:</b> this measure requires incremental training of node embedding models
+                    of networks of subsequent years. Node embeddings are obtained by the node2vec algorithm described in
+                    [2]. The score between two years is computed by averaging node embeddings to represent a network and
+                    taking cosine similarity between obtained network vectors. The score ranges from 0 to 1.
+                  </li>
+                </ul>
               </b-card-text>
               <img src='@/assets/timeseries.png'
                    width='800'
