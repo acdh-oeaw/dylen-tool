8b0ceea5983873f90a57f23c453db48a0f28578d
Seungbin Yim <liam21c@gmail.com>
2021-11-16 10:48:08 +0100

    Change selectedsubcorpus upon corpus change

--- src/store/store.js
+++ src/store/store.js
@@ -350,6 +350,7 @@ const mainModule = {
             } else {
                 state[payload.pane].selectedCorpus = state.availableCorpora[0];
             }
+            state[payload.pane].selectedSubcorpus = state.availableSourcesByCorpus[state[payload.pane].selectedCorpus.id][0];
         },
         changeSelectedSubcorpus(state, payload) {
             state[payload.pane].selectedSubcorpus = payload.subcorpus ? payload.subcorpus : state.availableSourcesByCorpus[state[payload.pane].selectedCorpus.id][0];
