93dfc4c1ae526346cd150dbd0fd1a10b10407087
Seungbin Yim <liam21c@gmail.com>
2022-01-17 16:55:18 +0100

    Extract sauto

--- src/store/store.js
+++ src/store/store.js
@@ -24,6 +24,7 @@ import {speakers_not_fount} from "@/helpers/speakers_not_found";
 import {getEgoNetworkTimeSeries, getGeneralTimeSeries} from "@/helpers/api_client";
 import {compare} from "@/helpers/utils";
 import {GENERAL_PARTY} from "@/helpers/mixins";
+import {sautoModule} from "@/store/sauto";
 
 Vue.component('b-spinner', BSpinner);
 
@@ -821,77 +822,6 @@ const mainModule = {
   }
 };
 
-const sautoModule = {
-  namespaced: true,
-  state: {
-    connection: null,
-    lastOverElement: null,
-    sauto: false,
-    size: null,
-    currentDrag: null
-  },
-  actions: {
-    setBoundingClientRect({ state }, { size }) {
-      state.size = size;
-    },
-    async connect({ state }) {
-      if (state.sauto) {
-        logger.log(props.sautoURI);
-        state.connection = new WebSocket(props.sautoURI);
-        state.connection.onerror = function(event) {
-          logger.error(event);
-        };
-        state.connection.onopen = function() {
-          logger.log('Connection with Sauto backed established successfully.');
-          state.connection.send(
-            JSON.stringify({
-              appVersion: props.appVersion,
-              type: 'VersionInfo'
-            })
-          );
-        };
-      }
-    },
-    agree({ state }, { agreed }) {
-      state.sauto = agreed;
-      if (agreed) {
-        this.dispatch('sauto/connect');
-      }
-    },
-    async handleMouseMove({ state }, { movement }) {
-      //send mouse positions
-      movement.type = 'MousePosition';
-      state.connection.send(JSON.stringify(movement));
-    },
-    async handleMouseOver({ state }, { mouseOver }) {
-      state.connection.send(JSON.stringify(mouseOver));
-    },
-    async handleKeyPress({ state }, { keyPress }) {
-      state.connection.send(JSON.stringify(keyPress));
-    },
-    async handleResize({ state }, { resized }) {
-      if (resized.paneId !== undefined) {
-        resized.type = 'Resize';
-        state.connection.send(JSON.stringify(resized));
-      }
-    },
-    async handleMouseClick({ state }, { click }) {
-      //send mouse click
-      click.type = 'MouseClick';
-      state.connection.send(JSON.stringify(click));
-    },
-    async handleDrag({ state }, { drag }) {
-      //send mouse drag and drop
-      drag.type = 'Drag';
-      state.connection.send(JSON.stringify(drag));
-    },
-    async handleScroll({ state }, { scroll }) {
-      //send mouse scroll
-      scroll.type = 'Scroll';
-      state.connection.send(JSON.stringify(scroll));
-    }
-  }
-};
 const store = new Vuex.Store({
   modules: {
     main: mainModule,
