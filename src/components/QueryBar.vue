c67cb8c048d19071656da1ec79b028d61fda393c
Katharina Wünsche <katharina_wunsche@yahoo.de>
2022-01-27 18:44:46 +0100

    eliminate vertical scroll bar in querybar

--- src/components/QueryBar.vue
+++ src/components/QueryBar.vue
@@ -1,28 +1,34 @@
 <template>
-  <b-row xl='12'>
-    <b-col xl='12'>
+  <b-row
+    xl='12'
+    class="w-100 mx-auto"
+  >
+    <b-col
+      xl='12'
+      class="pl-2"
+    >
       <b-row xl='12'>
         <b-col
-            data-sauto-id='search-form-1'
-            class='pb-1'
-            xl='12'
-            align-self='stretch'
+          data-sauto-id='search-form-1'
+          class='pb-1'
+          xl='12'
+          align-self='stretch'
         >
           <b-row
-              class='h-100'
-              align-h='center'
-              align-v='stretch'
+            class='h-100'
+            align-h='center'
+            align-v='stretch'
           >
             <b-col>
               <b-row>
                 <b-col v-if="type===EGO_NETWORK">
                   <b-card class='mt-2'>
                     <search-form
-                        :is-vertical='true'
-                        :with-labels='false'
-                        :pane="'pane' + 1"
-                        :is-sidebar='false'
-                        @visualizeClicked='validateFormVisibility'
+                      :is-vertical='true'
+                      :with-labels='false'
+                      :pane="'pane' + 1"
+                      :is-sidebar='false'
+                      @visualizeClicked='validateFormVisibility'
                     >
                     </search-form>
                   </b-card>
@@ -30,24 +36,27 @@
                 <b-col v-if="type===GENERAL_PARTY">
                   <b-card class='mt-2 px-0'>
                     <search-form-general
-                        :is-vertical='true'
-                        :with-labels='false'
-                        :pane="'pane' + 1"
-                        :is-sidebar='false'
-                        @visualizeClicked='validateFormVisibility'
+                      :is-vertical='true'
+                      :with-labels='false'
+                      :pane="'pane' + 1"
+                      :is-sidebar='false'
+                      @visualizeClicked='validateFormVisibility'
                     >
                     </search-form-general>
                   </b-card>
 
                 </b-col>
-                <b-col v-if="type===GENERAL_SPEAKER" class='mt-2'>
+                <b-col
+                  v-if="type===GENERAL_SPEAKER"
+                  class='mt-2'
+                >
                   <b-card>
                     <search-form-general-speaker
-                        :is-vertical='true'
-                        :with-labels='false'
-                        :pane="'pane' + 1"
-                        :is-sidebar='false'
-                        @visualizeClicked='validateFormVisibility'
+                      :is-vertical='true'
+                      :with-labels='false'
+                      :pane="'pane' + 1"
+                      :is-sidebar='false'
+                      @visualizeClicked='validateFormVisibility'
                     >
                     </search-form-general-speaker>
                   </b-card>
@@ -58,53 +67,59 @@
           </b-row>
         </b-col>
       </b-row>
-      <b-row xl='12' class="mt-3">
+      <b-row
+        xl='12'
+        class="mt-3"
+      >
         <b-col
-            data-sauto-id='search-form-2'
-            class='pb-1'
-            xl='12'
-            align-self='stretch'
+          data-sauto-id='search-form-2'
+          class='pb-1'
+          xl='12'
+          align-self='stretch'
         >
           <b-row
-              xl='12'
-              class='h-100'
-              align-v='center'
+            xl='12'
+            class='h-100'
+            align-v='center'
           >
             <b-col
-                xl='12'
-                v-if='!secondFormVisibility'>
+              xl='12'
+              v-if='!secondFormVisibility'
+            >
               <b-button
-                  pill
-                  data-sauto-id='second-query-button'
-                  size='sm'
-                  variant='secondary'
-                  v-if='secondFormVisibility'
-                  v-on:click='queryButtonClicked(2)'
+                pill
+                data-sauto-id='second-query-button'
+                size='sm'
+                variant='secondary'
+                v-if='secondFormVisibility'
+                v-on:click='queryButtonClicked(2)'
               >
                 <b>New Query</b>
               </b-button>
             </b-col>
             <b-col
-                xl='12'
-                v-if="secondFormVisibility && type===EGO_NETWORK">
+              xl='12'
+              v-if="secondFormVisibility && type===EGO_NETWORK"
+            >
               <b-card>
                 <search-form
-                    :with-labels='false'
-                    :pane="'pane' + 2"
-                    :is-sidebar='false'
+                  :with-labels='false'
+                  :pane="'pane' + 2"
+                  :is-sidebar='false'
                 >
                 </search-form>
               </b-card>
 
             </b-col>
             <b-col
-                xl='12'
-                v-if="secondFormVisibility && type===GENERAL_PARTY">
+              xl='12'
+              v-if="secondFormVisibility && type===GENERAL_PARTY"
+            >
               <b-card>
                 <search-form-general
-                    :with-labels='false'
-                    :pane="'pane' + 2"
-                    :is-sidebar='false'
+                  :with-labels='false'
+                  :pane="'pane' + 2"
+                  :is-sidebar='false'
                 >
                 </search-form-general>
               </b-card>
@@ -113,9 +128,9 @@
             <b-col v-if="secondFormVisibility && type===GENERAL_SPEAKER">
               <b-card>
                 <search-form-general-speaker
-                    :with-labels='false'
-                    :pane="'pane' + 2"
-                    :is-sidebar='false'
+                  :with-labels='false'
+                  :pane="'pane' + 2"
+                  :is-sidebar='false'
                 >
                 </search-form-general-speaker>
               </b-card>
@@ -133,11 +148,11 @@
 import SearchForm from '@/components/SearchForm';
 import SearchFormGeneral from '@/components/SearchFormGeneral';
 import SearchFormGeneralSpeaker from '@/components/SearchFormGeneralSpeaker';
-import {networkTypeMixin} from "@/helpers/mixins";
+import { networkTypeMixin } from '@/helpers/mixins';
 
 export default {
   mixins: [networkTypeMixin],
-  name: "QueryBar",
+  name: 'QueryBar',
   props: [],
   components: {
     SearchForm,
@@ -147,31 +162,32 @@ export default {
   data() {
     return {
       firstForm: true,
-      secondForm: false,
+      secondForm: false
     };
   },
   mounted() {
     this.$root.$on('visualizeClicked', () => {
-      let numberOfVisualisedNetworks =  this.$store.getters['main/numberOfNetworksVisualised']
-      if(numberOfVisualisedNetworks > 0) {
-        this.secondForm = true
+      let numberOfVisualisedNetworks =
+        this.$store.getters['main/numberOfNetworksVisualised'];
+      if (numberOfVisualisedNetworks > 0) {
+        this.secondForm = true;
       }
-    })
+    });
   },
   computed: {
     type() {
       return this.$store.getters['main/topNav'].networkType;
     },
     secondFormVisibility() {
-      return this.secondForm
+      return this.secondForm;
     }
-
   },
   methods: {
     validateFormVisibility() {
-      let numberOfVisualisedNetworks =  this.$store.getters['main/numberOfNetworksVisualised']
-      if(numberOfVisualisedNetworks > 0) {
-        this.secondForm = true
+      let numberOfVisualisedNetworks =
+        this.$store.getters['main/numberOfNetworksVisualised'];
+      if (numberOfVisualisedNetworks > 0) {
+        this.secondForm = true;
       }
     },
     queryButtonClicked(button) {
@@ -182,9 +198,8 @@ export default {
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
