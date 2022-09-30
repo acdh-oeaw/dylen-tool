72376021531c5c03a40adbc59f83f4dd13e05ca9
Dalibor Pančić <dalibor.pancic@oeaw.ac.at>
2022-09-30 18:07:45 +0200

    Update properties_local.js

--- src/properties/properties_local.js
+++ src/properties/properties_local.js
@@ -1,6 +1,5 @@
 export default {
   appVersion: 3.0,
-  sautoURI: 'ws://localhost:5000/app',
-  graphqlEndpoint:
-    'http://localhost:5000/graphql'
+  sautoURI: 'wss://sauto.acdh-dev.oeaw.ac.at/app',
+  graphqlEndpoint: 'https://dylen-ego-network-service.acdh-dev.oeaw.ac.at/graphql'
 };
